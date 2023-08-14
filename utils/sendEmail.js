47986ec624fb4187a295cfd8ec8e66eabb8e5130
Sundram Agarwal <agarwal.sundaram@gmail.com>
2023-08-14 12:36:14 +0530

    commit just

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -3,24 +3,24 @@ const nodemailer = require("nodemailer");
 const sendEmail = async (subject, message, send_to, sent_from, reply_to) => {
   // Create Email Transporter
   const transporter = nodemailer.createTransport({
-    // host: process.env.EMAIL_HOST,
-    // port: 587,
+    host: process.env.EMAIL_HOST,
+    port: 587,
     // secure: false,
-    service: "gmail",
+    // service: "gmail",
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
-    // tls: {
-    //   rejectUnauthorized: false,
-    // },
+    tls: {
+      rejectUnauthorized: false,
+    },
   });
 
   // Option for sending email
   const options = {
     from: sent_from,
     to: send_to,
-    // replyTo: reply_to,
+    replyTo: reply_to,
     subject: subject,
     html: message,
   };
